83446993b82e8db963f7ae705995219d7c61487f
lewiskk016 <lewiskk016@gmail.com>
2023-03-28 08:52:31 -0400

    sudoku

---
+++ board.rb
@@ -0,0 +1,84 @@
+class Board
+  attr_accessor :cups
+
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
+  end
+
+  def place_stones
+    @cups.each_with_index do |cup, idx|
+      next if idx == 6 || idx == 13
+      4.times do
+        cup << :stone
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+
+    idxx = start_pos
+    until stones.empty?
+      idxx += 1
+      idxx = 0 if idxx > 13
+
+      if idxx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif idxx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[idxx] << stones.pop
+      end
+    end
+
+    render
+    next_turn(idxx)
+  end
+
+  def next_turn(ending_cup_idx)
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+    ending_cup_idx
+    end
+    # helper method to determine what #make_move returns
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    @cups[0..6].all? { |cup| cup.empty? } || @cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+    player1_count = @cups[6].count
+    player2_count = @cups[13].count
+
+    if player1_count == player2_count
+      return :draw
+    elsif player1_count > player2_count
+          @name1
+    else
+      @name2
+    end
+  end
+end
